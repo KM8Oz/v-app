1013e944f1c910c75debe0b1de4fa786fb8db4cf
km8oz <ouazmir@gmail.com>
2022-04-13 22:52:44 +0300

    number to text Montant total

---
+++ src/render/components/CustomTextInput0.tsx
@@ -0,0 +1,63 @@
+import { AutoComplete } from 'antd';
+import { AutoCompleteProps } from 'antd/lib/auto-complete';
+import React, { ChangeEventHandler, ReactElement, useEffect } from 'react'
+import { InputProps } from 'react-select'
+import styled from 'styled-components';
+import { autocomplete, makeid } from '../tools';
+
+interface Props {
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    // suggestions:
+    onchange: any
+    value: string,
+    typeText: typeText,
+    maxchars?: number,
+    placeholder: string,
+    suggestions?: string[],
+    options: any[]
+    // classForAuto?:string
+}
+enum typeText {
+    normal = "v-normal-text",
+    small = "v-small-text",
+}
+function CustomTextInput0({ x, y, options, placeholder, suggestions = [], width, maxchars, height, value, typeText, onchange, ...rest }: Props&AutoCompleteProps): ReactElement {
+    const uniqueId = makeid(8);
+    useEffect(() => {
+        const isAuto = suggestions.length > 0;
+        if (isAuto) {
+            autocomplete(uniqueId, suggestions);
+        }
+    }, [suggestions])
+    return (
+        <foreignObject x={x} y={y} width={width} height={height}>
+            <StyledAutoComplete
+             {...rest}
+                options={options}
+                filterOption={(inputValue, option) =>
+                    option!.value.toUpperCase().search(inputValue.toUpperCase()) !== -1
+                }
+                onClick={inputValue => {
+                    //    console.log(e.currentTarget);
+                    inputValue.currentTarget.nodeValue = ""
+                }}
+                placeholder={placeholder} value={value} maxTagTextLength={maxchars || 15}
+                className={`v-text-input ${typeText} ${uniqueId}`} onChange={onchange} />
+        </foreignObject>
+    )
+}
+const StyledAutoComplete = styled(AutoComplete)`
+      .ant-select-selector{
+          border:unset !important;
+          box-shadow: unset !important;
+          background: transparent !important;
+          height: 20px !important;
+      }
+      input{
+          cursor: pointer !important;
+      }
+`;
+export { CustomTextInput0, typeText }
