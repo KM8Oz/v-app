b5f95f7002cc0f5789f18aa15807417894b58a4b
km8oz <ouazmir@gmail.com>
2022-05-04 23:25:58 +0300

    all

--- src/render/page/app.tsx
+++ src/render/page/app.tsx
@@ -10,12 +10,12 @@
     * - Author          : 
     * - Modification    : 
 **/
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { MemoryRouter as Router, Switch, Route, useLocation, useHistory } from 'react-router-dom';
 // import icon from '../../assets/icon.svg';
 import './app.less';
-import { Login, Corp }  from "@render/components";
+import { Login, Corp } from "@render/components";
 import { PersistentStoreProvider, usePersistentStore } from '@render/store';
 
 // import env from "react-dotenv";
@@ -40,19 +40,19 @@ export default function App() {
   //  },[])
   return (
     <PersistentStoreProvider>
-    <Router initialIndex={1} initialEntries={[
-      {pathname:"/", key:"/"},
-      {pathname:"/login", key:"/login"},
-      {pathname:"/Home", key:"/Home"}
-    ]} >
-      <Switch>
-      <Route  exact path="/"  component={Login} key={"/"}/>
-      <Route  exact path="/login" component={Login} key={"/login"}/>
-      </Switch>
+      <Router initialIndex={1} initialEntries={[
+        { pathname: "/", key: "/" },
+        { pathname: "/login", key: "/login" },
+        { pathname: "/Home", key: "/Home" }
+      ]} >
         <Switch>
-        <Route exact path="/Home" component={Corp}  key={"/Home"}/>
-      </Switch>
-    </Router>
+          <Route exact path="/" component={Login} key={"/"} />
+          <Route exact path="/login" component={Login} key={"/login"} />
+        </Switch>
+        <Switch>
+          <Route exact path="/Home" component={Corp} key={"/Home"} />
+        </Switch>
+      </Router>
     </PersistentStoreProvider>
   );
 }
